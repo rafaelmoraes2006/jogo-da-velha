d411f41bcb506aaa1cdd85152f05b5d960319620
rafaelmoraes2006 <168312406+rafaelmoraes2006@users.noreply.github.com>
2024-04-27 20:40:00 -0300

    Initial commit

---
+++ app/src/main/java/com/rafaelmoraes2006/jogo_da_velha/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rafaelmoraes2006.jogo_da_velha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
